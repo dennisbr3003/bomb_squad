9d1ef068561864fe608c6fd531190fa01a4aa81f
Dennis Brink <dennis.mjknd@gmail.com>
2022-12-02 21:05:07 +0100

    Runnable/handler/tags

--- app/src/main/java/com/dennis_brink/android/bombsquad/MainActivity.java
+++ app/src/main/java/com/dennis_brink/android/bombsquad/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
     GridLayout grid; // has to be androidx variant
 
     int score=0;
+
+    Runnable runnable;
+    Handler handler;
+
+    ImageView[] explosivesArray;
+
+    MediaPlayer mediaPlayer;
 
     private static final String TAG = "DENNIS_B";
 
@@ -42,6 +53,12 @@ public class MainActivity extends AppCompatActivity {
         imageViewGrenade8 = findViewById(R.id.grenade8);
         imageViewGrenade9 = findViewById(R.id.grenade9);
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.explosion_6288);
+
+        explosivesArray = new ImageView[] {imageViewGrenade1, imageViewGrenade2, imageViewGrenade3,
+                                           imageViewGrenade4, imageViewGrenade5, imageViewGrenade6,
+                                           imageViewGrenade7, imageViewGrenade8, imageViewGrenade9} ;
+
         grid = findViewById(R.id.grid);
 
         new CountDownTimer(5000, 1000) {
@@ -54,11 +71,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
 
-                // if done we do...
-                textViewCountdown.setVisibility(View.INVISIBLE);
-                textViewTime.setVisibility(View.VISIBLE);
-                textViewScore.setVisibility(View.VISIBLE);
-                grid.setVisibility(View.VISIBLE);
+                explosiveControl();
 
                 // start init game duration (30 secs counting down)
                 new CountDownTimer(30000, 1000) {
@@ -82,6 +95,42 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "view " + view.getTag());
         score++;
         textViewScore.setText("Score: " + score);
+
+        Log.d(TAG, ""+Integer.parseInt(view.getTag().toString()));
+        ImageView v = explosivesArray[Integer.parseInt(view.getTag().toString()) - 1];
+        v.setImageResource(R.drawable.boom3);
+
+        // start audiofile
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.seekTo(0); // if sound already playing then set back to 0 (cut off playing sound ad start new one)
+        }
+        mediaPlayer.start();
+
+    }
+
+    public void explosiveControl(){
+        // if done we do...
+        textViewCountdown.setVisibility(View.INVISIBLE);
+        textViewTime.setVisibility(View.VISIBLE);
+        textViewScore.setVisibility(View.VISIBLE);
+
+        handler = new Handler();
+        runnable = () -> {
+            for(ImageView explosive: explosivesArray){
+                explosive.setVisibility(View.INVISIBLE);
+                explosive.setImageResource(R.drawable.granade); // set the images back to explosive
+            }
+            grid.setVisibility(View.VISIBLE);
+            Random random = new Random();
+            int i = random.nextInt(explosivesArray.length);
+            explosivesArray[i].setVisibility(View.VISIBLE);
+
+            handler.postDelayed(runnable, 2000); // this will setup the repeat sequence
+
+        };
+
+        handler.post(runnable); // this will start the runnable and in fact start the game
+
     }
 
 }
